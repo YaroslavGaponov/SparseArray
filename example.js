7daf1875737be431ac53530558e4ae957c952933
Yaroslav Gaponov <yaroslav.gaponov@avid.com>
2015-07-07 07:20:27 -0400

    init

---
+++ example.js
@@ -0,0 +1,11 @@
+var SparseArray = require('./sparsearray');
+
+var sa = new SparseArray();
+for(var i=0; i<1000000; i+=100) {
+    sa.put(i,'hello'+i);
+}
+
+console.log(sa.get(300));
+console.log(sa.remove(300));
+console.log(sa.get(300));
+console.log(sa.get(5000));
